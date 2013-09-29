ca8a52f061e7587b2c0c27a57d8b880cf4cb7ebc
Michael Saltzman <mik854@gmail.com>
2013-09-29 07:51:20 -0400

    Backend support for Blurred Lines.

--- generateaudio.py
+++ generateaudio.py
@@ -1,10 +1,12 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-def generateAudio(lyrics):
+def generateAudio(lyrics, n):
     '''Takes in the lyrics. Returns a URL to the autotuned mp3 (no backing).'''
 
-    s = lyrics.replace (" ", "+")
+    s = lyrics.replace (" ", "%20")
+    s = s.replace (".", "%0A")
+    s = s.replace (",.", "%2C")
 
     tts_url = "http://tts-api.com/tts.mp3?q=%s&return_url=1" % s
     tts = urllib2.urlopen(tts_url)
@@ -13,20 +15,24 @@ def generateAudio(lyrics):
     input_file = soup.get_text()
 
     access_id = "78908f19-88ab-4e11-ab95-ac47aecd8458"
-
-    midi_pitches = "58-58-58-58-58-58-57-57-53-57-55-53-58-57-57-53-57-55-" \
-        "53-58-57-57-53-53-57-55-55-53-57-55-53-58-53-53-53-57-55-55-53-58-..."
-
+    if n == 0:
+        midi_pitches = "58-58-58-58-58-58-57-57-53-57-55-53-58-57-57-53-57" \
+        "-55-53-58-57-57-53-53-57-55-55-53-57-55-53-58-53-53-53-57-55-55-53" \
+        "-58-..."
+        tempo_factor = "0.75"
+    else:
+        midi_pitches = "50-..."
+        tempo_factor = "1"
 
     sonic_url = "https://api.sonicapi.com/process/elastiqueTune" \
         "?access_id=%s" \
         "&input_file=%s" \
-        "&tempo_factor=0.75" \
+        "&tempo_factor=%s" \
         "&pitchcorrection_percent=100" \
         "&pitchdrift_percent=0" \
-        "&midi_pitches=%s" % (access_id, input_file, midi_pitches) 
+        "&midi_pitches=%s" % (access_id, input_file, tempo_factor, midi_pitches) 
 
-    #print sonic_url
+    print sonic_url
     '''
     req2 = urllib2.Request(sonic_url)
     response = urllib2.urlopen(req2)
@@ -40,5 +46,5 @@ if __name__=='__main__':
     lyrics = "This+won't+work+if+there+are+spaces+in+the+url+you+provide."
     lyrics2 = "I came in like a wrecking ball. I never hit so hard in love. All I wanted was to break your walls. All you ever did was wreck me. Yeah, you, you wreck me."
 
-    generateAudio(lyrics2)
+    generateAudio(lyrics2, 1)
     
