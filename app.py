59b1f10537beee40679a8604b30bb6365bd1227b
Lisa Li <ll2768@columbia.edu>
2013-09-29 06:25:32 -0400

    moar edits

--- app.py
+++ app.py
@@ -35,8 +35,8 @@ def hello_world():
 				else:
 					print "Status Code is not 200."
 			### Call Justin's Poem Stuff                                      
-			lyrics = Markup("<br>".join(PG.getPoem(title_list)))
-			web_url = generateAudio("".join(PG.getPoem(title_list)))
+			lyrics = Markup(".<br>".join(PG.getPoem(title_list))+"." )
+			web_url = generateAudio(" ".join(PG.getPoem(title_list)))
 			return render_template('results.html', lyrics=lyrics, web_url=web_url)
 		else:
 			print "Error with API Key config."
