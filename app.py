ab35def740efa8d3478b8ffab69e1c1c01935222
Lisa Li <ll2768@columbia.edu>
2013-09-29 01:47:26 -0400

    we good

--- app.py
+++ app.py
@@ -5,28 +5,33 @@ import requests
 app = Flask(__name__)
 
 app.config['NYTIMES_API_KEY'] = "4b7f1044c821ae86af829a6a53f4e1c8:11:68190060"
-app.config['API_PATH'] = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=economy&api-key="
+app.config['API_PATH'] = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=economy&page="
+app.config['API_PATH2']="&api-key="
 
 @app.route('/', methods=['GET'])
 def hello_world():
 	loc = 0;
 	if app.config['NYTIMES_API_KEY']:
-		api_request = requests.get(app.config['API_PATH'] + app.config['NYTIMES_API_KEY'])
-		if api_request.status_code == 200:
-			json_response = api_request.json()
-			if json_response:
-				title_list = []
-				for x in range (0,9):
-					article_title = json_response['response']['docs'][x]['headline']['main']
-					title_list.append(article_title)
-					print article_title
-#				return jsonify(json_response)
+		for i in range(0, 10):
+			app.config['API_PAGE'] = str(i)
+			api_request = requests.get(app.config['API_PATH'] + app.config['API_PAGE'] + app.config['API_PATH2']+app.config['NYTIMES_API_KEY'])
+#			print api_request
+			if api_request.status_code == 200:
+				json_response = api_request.json()
+				if json_response:
+					title_list = []
+					for x in range (0,10):
+						article_title = json_response['response']['docs'][x]['headline']['main']
+						title_list.append(article_title)
+						print "TITLE#"+str(i)+", "+str(x)+": "+article_title
+#				       		return jsonify(json_response)
+				else:
+					print "json_response no"
 			else:
-				print "json_response no"
-		else:
-			print "api_request no"
+				print "api_request no"
 	else:
 		print "config error"
+	return "we good"
 
 if __name__ == '__main__':
 	app.run(debug=True)
