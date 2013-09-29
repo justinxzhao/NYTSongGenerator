ae69e5ff50bfbe25a089d844162668e71dadfd30
Justin Zhao <justin.zhao71@gmail.com>
2013-09-28 23:28:14 -0400

    All lyric hashing functions finished

--- helper/dictionaryCreator.py
+++ helper/dictionaryCreator.py
@@ -1,5 +1,13 @@
 """ This helper script builds the dictionary for number of words and their syllables """
 import re
+from collections import defaultdict
+
+class Word:
+
+	def __init__(self, word, syllables, rhyme):
+		self.syllables = syllables
+		self.rhyme = rhyme
+		self.word = word
 
 f = open('cmudict.0.7a.txt')
 
@@ -7,12 +15,52 @@ f = open('cmudict.0.7a.txt')
 lines = f.readlines()
 f.close()
 
-dictionary = {}
+# create the dictionaries
+dictByWord = {}
+dictByRhyme = defaultdict(list)
+dictBySyllable = defaultdict(list)
+
+# for all the words in the dictionary (one per line), find stats and add them to corresponding dictionaries
 for str in lines:
 	word = re.split('[ ]', str)[0]
-	charArray = list(str)
-	numSyllables = len ( filter(lambda x: x in '1234567890', charArray) )
-	dictionary[word] = numSyllables
+	rhyme = filter( lambda x: x not in '1234567890', re.split('[ ]', str)[-1] )
 
-def getDictionary():
-	return dictionary
+	# the number of numbers indicates the number of syllables
+	syllable = len ( filter(lambda x: x in '1234567890', list(str)) )
+	
+	w = Word(word, syllable, rhyme)
+
+	dictByWord[word] = w
+	dictByRhyme[rhyme].append(w)
+	dictBySyllable[syllable].append(w)
+
+""" Print dictByWord
+for entry in dictByWord:
+	print dictByWord[entry].word
+	print dictByWord[entry].rhyme
+"""
+
+""" Print dictByRhyme
+for entry in dictByRhyme:
+	table = dictByRhyme[entry]
+	print "New Rhyme: " + entry
+	for word in table:
+		print word.word
+"""
+
+""" Print dictBySyllable
+for entry in dictBySyllable:
+	table = dictBySyllable[entry]
+	print "Syllable Count: " + entry
+	for word in table:
+		print word.word
+"""
+
+def getDictByWord():
+	return dictByWord
+
+def getDictByRhyme():
+	return dictByRhyme
+
+def getDictBySyllable():
+	return dictBySyllable
